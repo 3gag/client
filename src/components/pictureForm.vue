2c12b48cd8d5111a31498f84eb8e47f4a68adf20
laskarks <laskar.ksatria12@gmail.com>
2019-10-10 19:06:23 -0400

    Misah top-menu,sidebar,pictureContent,formAddPicture

---
+++ src/components/pictureForm.vue
@@ -0,0 +1,36 @@
+<template>
+        
+ <div>
+    <div class="content-title">
+            <h4>Add New Picture</h4>
+    </div>
+    <div class="content">
+            <form action="">
+                <input type="text" id="pic-title">
+                <input type="file" name="pic" accept="image/*">
+                <input type="submit" value="Upload Picture">
+            </form>
+    </div>
+  <div>
+      
+</template>
+
+
+<script>
+export default {
+    data(){
+
+    },
+    methods(){
+
+    },
+
+}
+</script>
+
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/topNavbar.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+    <div class="hamburger">
+        <div class="one"></div>
+        <div class="two"></div>
+        <div class="three"></div>
+    </div>
+    <div class="top_menu">
+    <div class="logo">3Gags</div>
+        <ul>
+           <li>
+             <a href="#"><i class="fas fa-search"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-bell"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-user"></i></a>
+            </li>
+        </ul>
+    </div>
+     <div class="top_menu">
+        <div class="logo">3Gags</div>
+        <ul>
+            <li>
+                <a href="#"><i class="fas fa-search"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-bell"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-user"></i></a>
+            </li>
+        </ul>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        
+    }
+}
+</script>
