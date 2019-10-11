954be7182a466080de999468d20e0123e8c88023
uzaisindiko <uzai.sindiko@gmail.com>
2019-10-11 10:47:05 +0700

    login register

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <login
+      v-if="page === 'login'"
+      @toRegister="changePages('register')"
+      @toDashboard="changePages('dashboard')"
+    ></login>
+    <register
+      v-if="page === 'register'"
+      @toLogin="changePages('login')"
+      @toDashboard="changePages('dashboard')"
+    ></register>
+    <dashboard v-if="page === 'dashboard'"></dashboard>
+  </div>
+</template>
+
+<script>
+import login from "./components/login";
+import register from "./components/register";
+import dashboard from "./components/dashboard";
+
+export default {
+  components: {
+    login,
+    register,
+    dashboard
+  },
+  data() {
+    return {
+      page: "login"
+    };
+  },
+  methods: {
+    changePages(page) {
+      this.page = page;
+    }
+  },
+  created() {
+    if (localStorage.getItem("token")) {
+      this.page = "dashboard";
+    } else {
+      this.page = "login";
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/register.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="register">
+    <h1>register</h1>
+    <p>(ง'̀-'́)ง</p>
+    <input v-model="user.name" type="text" placeholder="Enter Name" />
+    <input v-model="user.email" type="text" placeholder="Enter Email" />
+    <input v-model="user.password" type="password" placeholder="Enter Password" />
+    <button @click.prevent="register" class="btn-register">register</button>
+    <a @click.prevent="toLogin">Login</a>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      user: {
+        name: "",
+        email: "",
+        password: ""
+      },
+      server: `http://localhost:3000`
+    };
+  },
+  methods: {
+    toLogin() {
+      this.$emit("toLogin");
+    },
+    register() {
+      axios({
+        method: "post",
+        url: `${this.server}/user/register`,
+        data: {
+          name: this.user.name,
+          email: this.user.email,
+          password: this.user.password
+        }
+      })
+        .then(({ data }) => {
+          localStorage.setItem("token", data.token);
+          this.$emit("toDashboard");
+        })
+        .catch(err => {
+          Swal.fire({
+            type: "error",
+            title: "Oops...",
+            text: `${err.response.data.message}`
+          });
+        });
+    }
+  }
+};
+</script>
+
+<style>
+.register {
+  background-color: rgba(255, 255, 255, 0.445);
+  height: 500px;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.18);
+}
+
+.register h1 {
+  font-size: 30px;
+  letter-spacing: 5px;
+  font-family: "Indie Flower", cursive;
+  text-align: center;
+  border-bottom: 2px solid black;
+}
+
+.register p {
+  font-size: 40px;
+}
+
+.register input {
+  margin: 5px;
+  padding: 15px 70px 15px 5px;
+  border: none;
+  border-bottom: 0.6px solid rgb(90, 90, 90);
+}
+
+.btn-register {
+  padding: 9px 20px;
+  border: none;
+  background-color: rgb(12, 12, 12);
+  color: white;
+  cursor: pointer;
+  transition: 300ms;
+  text-align: center;
+}
+
+.btn-register:hover {
+  padding: 11px 22px;
+  box-shadow: 5px 5px 1px 0px rgba(0, 0, 0, 0.18);
+}
+
+.register a {
+  color: rgb(68, 15, 241);
+  margin-left: 75%;
+  cursor: pointer;
+  text-decoration: underline;
+}
+</style>
